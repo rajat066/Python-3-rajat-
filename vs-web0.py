bf5867a6d702fba2ac5e8e954fb10b0f4bc11cfa
RAJAT KUMAR UPADHYAYA <rajatupadhyaya066@gmail.com>
2023-02-01 17:12:08 +0530

    clear all

--- vs-web0.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-computers = pd.read_csv("C:/Users/rajat/Desktop/python/advanced python-infy/computers.csv")
-print(computers.head())
