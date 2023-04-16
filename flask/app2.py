a49d2cbce4ee7621c92adcc64ca8a2e17c35c8d9
RAJAT KUMAR UPADHYAYA <rajatupadhyaya066@gmail.com>
2023-04-16 14:33:22 +0530

    final commit on 16th April

--- flask/app2.py
+++ flask/app2.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import pyodbc
+from requests import session
 
 user = Flask(__name__)
 
@@ -12,6 +13,7 @@ def connection():
                       'Database=QuickKartDB;'
                       'Trusted_Connection=yes;')
     return conn
+
 # conn = pyodbc.connect('Driver={SQL Server};'
 #                       'Server=DESKTOP-4GHBHUH;'
 #                       'Database=QuickKartDB;'
@@ -41,8 +43,36 @@ def main():
 @user.route("/login/", methods=['GET', 'POST'])
 def login():
     # Output message if something goes wrong...
-    msg = 'Something went wrong'
-    return render_template('login.html', msg='')
+    error_msg = 'Something went wrong'
+    action = render_template('login.html', msg='')
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        #Check if account exists  using MSSql
+        conn = connection()
+        cursur = conn.cursor()
+        cursur.execute('SELECT * FROM Users WHERE EmailId = ? AND UserPassword = ?', (username, password))
+        #Fetch one record and return result
+        account = cursur.fetchone()
+        if account:
+            # Create session data, we can access this data in other routes
+            # session['loggedin'] = True
+            # session['id'] = account['id']
+            # session['username'] = account['username']
+            # Redirect to home page
+            action = redirect("/landing")
+            # msg1 = "Hello {0}".format(username)
+            return action
+        else:
+            #Account doent exist or username/password incorrect
+            msg = 'Incorrect username/password!'
+
+    return action
+
+@user.route("/landing/", methods=['GET', 'POST'])
+def landing1():
+    s
+
 
 @user.route("/contact/")
 def contact():
